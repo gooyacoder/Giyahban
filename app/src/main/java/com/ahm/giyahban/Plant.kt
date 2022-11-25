e1beff227527ebb59c20ded94703ef7227be4ea6
amirhosein <gooyacoder@gmail.com>
2022-11-25 12:52:13 +0330

    4_9_1401    12:50

---
+++ app/src/main/java/com/ahm/giyahban/Plant.kt
@@ -0,0 +1,11 @@
+package com.ahm.giyahban
+
+import java.util.ArrayList
+
+data class Plant(val plant_name: String,
+                 val image: ByteArray,
+                 val water_date: Long,
+                 val water_period: Int,
+                 val fertilizers: ArrayList<String>,
+                 val fertilizer_dates: ArrayList<Long>,
+                 val fertilizer_periods: ArrayList<Int> )  {}
