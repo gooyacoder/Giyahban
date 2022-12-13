166e088370fecd0cb22823b803c2e610ab91f91d
amirhosein <gooyacoder@gmail.com>
2022-12-13 16:14:38 +0330

    22_9_1401    16:14

--- app/src/main/java/com/ahm/giyahban/CustomPlantList2.kt
+++ app/src/main/java/com/ahm/giyahban/CustomPlantList2.kt
@@ -10,11 +10,12 @@ import android.widget.ImageView
 import android.widget.TextView
 
 
-
 class CustomPlantList2(
     private val context: Activity,
     private val plantNames: MutableList<String>,
-    private val plantImages: MutableList<Bitmap>) :
+    private val plantImages: MutableList<Bitmap>,
+    private val water: Boolean,
+    private val fertilizers: MutableList<String>) :
     ArrayAdapter<String>(context, R.layout.row_item, plantNames) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var row = convertView
@@ -22,10 +23,19 @@ class CustomPlantList2(
         if (convertView == null) row = inflater.inflate(R.layout.row_item, null, true)
         val textViewPlantName = row!!.findViewById<View>(R.id.textViewPlantName) as TextView
         val plantImage = row.findViewById<View>(R.id.imageViewPlant) as ImageView
-        val deleteButton = row.findViewById<View>(R.id.delete_button) as Button
-        deleteButton.setFocusable(false)
+        val wateringImage = row.findViewById<View>(R.id.watering_reminder_image) as ImageView
+        plantImage.setFocusable(false)
+        if(water){
+            wateringImage.visibility = View.VISIBLE
+        }
         textViewPlantName.text = plantNames[position]
         plantImage.setImageBitmap(plantImages[position])
+        val fertilizerList = row.findViewById<View>(R.id.fertilizer_tasks_list) as TextView
+        var frt_list: String = ""
+        for(frt in fertilizers){
+            frt_list += frt + ", "
+        }
+        fertilizerList.text = frt_list
         return row
     }
 }
--- app/src/main/java/com/ahm/giyahban/TodaysTasksActivity.kt
+++ app/src/main/java/com/ahm/giyahban/TodaysTasksActivity.kt
@@ -1,9 +1,18 @@
 package com.ahm.giyahban
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import java.util.*
+import kotlin.collections.HashMap
 
 class TodaysTasksActivity : AppCompatActivity() {
+
+    var TasksList : ListView? = null
+    var today: Long = 0
+    var water: Boolean = false
+    val fertilizerList : MutableList<String> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todays_tasks)
@@ -13,10 +22,62 @@ class TodaysTasksActivity : AppCompatActivity() {
     }
 
     private fun getTodaysTasks() {
-        TODO("Not yet implemented")
+        val db = DatabaseHelper(this)
+        val plants = db.getPlants()
+        db.close()
+
+        val plant_names_list : MutableList<String> = mutableListOf()
+        val plant_images_list : MutableList<Bitmap> = mutableListOf()
+
+
+        //plant_names_list.clear()
+        if (plants.size > 0) {
+            for (plant in plants) {
+                if(hasTask(plant)){
+                    val plant_image = DbBitmapUtility.getImage(plant.image)
+                    plant_images_list.add(plant_image)
+                    val plant_name = plant.plant_name
+                    plant_names_list.add(plant_name)
+
+                }
+            }
+        }
+        val customPlantList = CustomPlantList2(this, plant_names_list, plant_images_list, water, fertilizerList)
+        TasksList?.adapter = customPlantList
+    }
+
+    private fun hasTask(plant: Plant): Boolean {
+        val watering_time_passed = calculateDays(today - plant.water_date!!.toLong())
+        val fertilizer_time_passed : ArrayList<Int> = ArrayList()
+        if(plant.fertilizer_dates != null){
+            for (frt in plant.fertilizer_dates){
+                fertilizer_time_passed.add(calculateDays(today - frt.toLong()))
+            }
+            var i = 0
+            for(ft in fertilizer_time_passed){
+                if(ft == plant.fertilizer_periods?.get(i)?.toInt()){
+                    fertilizerList.add(plant.fertilizers?.get(i) ?: "")
+                    return true
+                }
+                i++
+            }
+        }
+        if(watering_time_passed == plant.water_period!!.toInt()){
+            water = true
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    private fun calculateDays(l: Long): Int {
+        return (l/(1000*60*60*24)).toInt()
     }
 
     private fun prepareUI() {
-        TODO("Not yet implemented")
+        TasksList = findViewById(R.id.tasks_list)
+        val calendar = Calendar.getInstance()
+        today = calendar.getTime().getTime()
     }
 }
