7234901b05edf6401c6daea92339402b1d981419
amirhosein <gooyacoder@gmail.com>
2023-08-08 18:19:13 +0330

    17_5_1402     18:19

--- app/src/main/java/com/ahm/giyahban/AllPlantsActivity.kt
+++ app/src/main/java/com/ahm/giyahban/AllPlantsActivity.kt
@@ -3,64 +3,42 @@ package com.ahm.giyahban
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.marginEnd
 
 class AllPlantsActivity : AppCompatActivity() {
-    var listView: ListView? = null
+    //var listView: ListView? = null
     var index: Int = 0
-    val plant_names_list : MutableList<String> = mutableListOf()
-    val plant_images_list : MutableList<Bitmap> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_plants)
-        listView = findViewById(R.id.list)
-        listView?.onItemClickListener = object : AdapterView.OnItemClickListener {
 
-            override fun onItemClick(parent: AdapterView<*>, view: View,
-                                     position: Int, id: Long) {
-                index = position
-            }
-        }
-    //    prepareUI()
+        prepareUI()
     }
 
-//    private fun prepareUI() {
-//        val db = DatabaseHelper(this)
-//        val plants = db.getPlants()
-//        db.close()
-//
-//        plant_names_list.clear()
-//        if (plants.size > 0) {
-//            for (plant in plants) {
-//                val plant_image = DbBitmapUtility.getImage(plant.image)
-//                plant_images_list.add(plant_image)
-//                val plant_name = plant.plant_name
-//                plant_names_list.add(plant_name)
-//
-//            }
-//        }
-//        val customPlantList = CustomPlantList(this, plant_names_list, plant_images_list)
-//        listView?.adapter = customPlantList
-//    }
+    private fun prepareUI() {
+        val db = DatabaseHelper(this)
+        val plants = db.getPlants()
+        db.close()
 
-    fun delete_button_clicked(view: View) {
-        val builder = AlertDialog.Builder(this)
-        val plantName = plant_names_list[index]
-        builder.setMessage("Are you sure you want to Delete the \"$plantName\"?")
-            .setCancelable(false)
-            .setPositiveButton("Yes") { dialog, id ->
-                val db = DatabaseHelper(this)
-                db.removePlant(plantName)
-                db.close()
-               // prepareUI()
+        val parentLayout = findViewById<ViewGroup>(R.id.linearLayoutAllPlants)
+        if (plants.size > 0) {
+            for (plant in plants) {
+                val plant_image = DbBitmapUtility.getImage(plant.image)
+                val plant_name = plant.plant_name
+                val inflater = LayoutInflater.from(this)
+                val childView = inflater.inflate(R.layout.all_plants_layout, parentLayout, false)
+                val imageView = childView.findViewById<ImageView>(R.id.plantImage)
+
+                imageView.setImageBitmap(plant_image)
+                val textView = childView.findViewById<TextView>(R.id.plantName)
+                textView.setText(plant_name)
+                parentLayout.addView(childView)
             }
-            .setNegativeButton("No") { dialog, id ->
-                // Dismiss the dialog
-                dialog.dismiss()
-            }
-        val alert = builder.create()
-        alert.show()
+        }
     }
 }
